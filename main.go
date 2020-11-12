ae3cd90e728f6899dc5851c34773a46c631e5fe8
Allison Piovani <apiovani25@gmail.com>
2020-11-11 23:02:18 -0300

    New route livros

--- main.go
+++ main.go
@@ -1,15 +1,57 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Livro struct {
+	Id     int
+	Titulo string
+	Autor  string
+}
+
+var Livros []Livro = []Livro{
+	Livro{
+		Id:     1,
+		Titulo: "O Guarani",
+		Autor:  "José de Alencar",
+	},
+	Livro{
+		Id:     2,
+		Titulo: "Cazuza",
+		Autor:  "Viriato Correia",
+	},
+	Livro{
+		Id:     3,
+		Titulo: "Dom Casmurro",
+		Autor:  "Machado de Assis",
+	},
+}
+
+func pageMain(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bem Vindo")
+}
+
+func listarLivros(w http.ResponseWriter, r *http.Request) {
+	encoder := json.NewEncoder(w)
+	encoder.Encode(Livros)
+}
+
+func confRoutes() {
+	http.HandleFunc("/", pageMain)
+	http.HandleFunc("/livros", listarLivros)
+}
+
+func confService() {
+	port := "1337"
+
+	confRoutes()
+	http.ListenAndServe(":"+port, nil)
+}
+
 func main() {
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Bem Vindo")
-	})
-
-	http.ListenAndServe(":1337", nil)
+	confService()
+	fmt.Println("Servidor esta rodando na porta")
 }
