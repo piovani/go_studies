9021a716971ad9f01171bf44ae39e801945b63dc
Allison Piovani <apiovani25@gmail.com>
2020-12-19 20:42:51 -0300

    User dotenv

--- database/db.go
+++ database/db.go
@@ -1,17 +1,28 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
 	"github.com/piovani/go_api/domain"
 )
 
 func Connection() *gorm.DB {
+	err := godotenv.Load()
 
-	dsn := "user:secret@tcp(127.0.0.1:33069)/database?charset=utf8&parseTime=True&loc=Local"
-	db, err := gorm.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Error during Load dotenv: %v", err)
+	}
+
+	db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_DATABASE")))
 
 	if err != nil {
 		log.Fatalf("Error during connection: %v", err)
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.3.0
 	github.com/satori/go.uuid v1.2.0
 	gorm.io/driver/mysql v1.0.3
 	gorm.io/gorm v1.20.8
