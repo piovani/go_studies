cb6b9a85fedaa3afe555902847fbb19dd9f101ef
Allison Piovani <apiovani25@gmail.com>
2021-10-21 20:11:33 -0300

    Securt save

--- example3/src/controllers/usuarios.go
+++ example3/src/controllers/usuarios.go
@@ -1,8 +1,29 @@
 package controllers
 
-import "net/http"
+import (
+	"api/modelos"
+	"api/modelos"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	corpoRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar()
+
+
+
+
 	w.Write([]byte("Criando Usuário!"))
 }
 
