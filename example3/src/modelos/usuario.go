c48fc8f505836d472aff91549db929c7a4868aae
Allison Piovani <apiovani25@gmail.com>
2021-10-23 19:04:00 -0300

    Validated data before save on database

--- example3/src/modelos/usuario.go
+++ example3/src/modelos/usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Usuario struct {
 	ID       uint64    `json:"id,omitempty"`
@@ -9,4 +13,39 @@ type Usuario struct {
 	Email    string    `json:""email,omitempty`
 	Senha    string    `json:""senha,omitempty`
 	CriadoEm time.Time `json:""criadoEm,omitempty`
+}
+
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O Nome é obrigatório")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("O Nick é obrigatório")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("O Email é obrigatório")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("O Senha é obrigatório")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
