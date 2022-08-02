3f0e56ccc1d8bfac4ebb73a9bcc2977a703c0ffc
Allison Piovani <apiovani25@gmail.com>
2022-08-02 08:16:48 -0300

    added: endpoint echo

---
+++ echo/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", hello)
+
+	e.Logger.Fatal(e.Start(":8081"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, world!")
+}
