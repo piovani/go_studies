57c17ff36568c1697dbb4a6dd94a0b4eb0a9a1d4
Allison Piovani <apiovani25@gmail.com>
2022-08-07 15:25:06 -0300

    changed: projects with problem

--- api_2/repositories/livro_repository.go
+++ api_2/repositories/livro_repository.go
@@ -1,14 +1,23 @@
 package repository
 
 import (
+	"log"
+
 	"github.com/piovani/go_api/domain"
+	"gorm.io/gorm"
 )
 
-type LivroRepository interface {
-	Insert(livro *domain.Livro) (*domain.Livro, error)
+type LivroRepository struct {
+	Db *gorm.DB
 }
 
-type (repo LivroRepository) Insert(livro *domain.Livro) (*domain.Livro, error) {
+func NewLicroRepository(db *gorm.DB) *LivroRepository {
+	return &LivroRepository{
+		Db: db,
+	}
+}
+
+func (repo LivroRepository) Insert(livro *domain.Livro) (*domain.Livro, error) {
 	err := repo.Db.Create(livro).Error
 
 	if err != nil {
