1fd58b4d809f827ee43193e24c7991961c0a2650
Allison Piovani <apiovani25@gmail.com>
2023-04-11 18:08:52 -0300

    added: send email

---
+++ email/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"gopkg.in/gomail.v2"
+)
+
+func main() {
+	user := ""
+	password := ""
+	msg := gomail.NewMessage()
+
+	msg.SetHeader("From", "from@example.com")
+	msg.SetHeader("To", "to@example.com")
+	msg.SetHeader("Subject", "Assunto Teste")
+	msg.SetBody("text/html", "<b>Corpo teste</b>")
+
+	n := gomail.NewDialer("sandbox.smtp.mailtrap.io", 587, user, password)
+
+	if err := n.DialAndSend(msg); err != nil {
+		fmt.Println("DEU RUIM: %v", err)
+		panic(err)
+	} else {
+		fmt.Println("DEU BOM!!!")
+	}
+}
