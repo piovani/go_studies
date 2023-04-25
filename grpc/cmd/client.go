3468e8895df79cde0d0a353a068b5702f2f930e8
Allison Piovani <apiovani25@gmail.com>
2023-04-25 20:27:35 -0300

    created: client grpc

--- grpc/cmd/client.go
+++ grpc/cmd/client.go
@@ -1,15 +1,38 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/piovani/go_studies/grpc/pb"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var Client = &cobra.Command{
 	Use: "client",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("CLIENT")
+		ExecuteClient()
 	},
 }
 
+func ExecuteClient() {
+	conn, err := grpc.Dial("localhost:9001", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+
+	client := pb.NewHelloClient(conn)
+
+	req := &pb.HelloRequest{
+		Name: "Marcos",
+	}
+
+	res, err := client.SayHello(context.Background(), req)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(res)
+}
